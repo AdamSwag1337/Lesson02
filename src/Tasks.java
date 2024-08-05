3f3786874a784360b5587011445a33dd7f7a850d
AdamSwag1337 <artem.podchernyaev@gmail.com>
2024-08-05 18:16:37 +0300

    New

---
+++ src/Tasks.java
@@ -0,0 +1,11 @@
+
+public class Tasks {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+}
+
+
