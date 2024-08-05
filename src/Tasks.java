e131a610d87061a811145aee1f3bd2520842a371
AdamSwag1337 <artem.podchernyaev@gmail.com>
2024-08-05 21:09:43 +0300

    New1

--- src/Tasks.java
+++ src/Tasks.java
@@ -6,6 +6,25 @@ public class Tasks {
         System.out.println("Apple");
     }
 
-}
+    public static void checkSumSign() {
+        int a = 5;
+        int b = -7;
+        int sum = a + b;
+        if (sum >= 0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицатиельная");
+    }
 
+    public static void printColor() {
+        int value = 333 ;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+}
 
